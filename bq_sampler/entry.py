65d5a21dfcab6458e4e6665ac10d6ededec0837c
Gustavo Kuhn Andriotti <gkandriotti@google.com>
2022-05-02 12:22:03 +0200

    Finished with unittests

--- bq_sampler/entry.py
+++ bq_sampler/entry.py
@@ -5,11 +5,13 @@ GCP `CloudFunction`_ entry point
 .. _CloudFunction: https://cloud.google.com/functions/docs/quickstart-python
 """
 # pylint: enable=line-too-long
+import base64
+from datetime import datetime
+import calendar
+import json
 from typing import Any, Dict, Optional
 import logging
 import os
-
-import flask
 
 from bq_sampler import process_request
 from bq_sampler import request_parser
@@ -34,15 +36,51 @@ def handler(  # pylint: disable=unused-argument
         Response object using
         `make_response <https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.make_response>`.
     """
-    _LOGGER.debug('Event: <%s>', event)
-    _LOGGER.debug('Context: <%s>', context)
-    _LOGGER.debug('Environment: %s', str(os.environ))
-    _LOGGER.info('Starting CSV upload')
-    event_request = request_parser.to_event_request(event)
+    _LOGGER.debug(
+        'Processing event ID <%s> from <%s> type <%s>',
+        context.event_id,
+        context.resource.get('name'),
+        context.resource.get('type'),
+    )
+    try:
+        _handler(event, context)
+    except Exception as err:
+        msg = f'Could not process event: <{event}>, context: <{context}>, and environment: <{os.environ}>'
+        _LOGGER.critical(msg)
+        raise RuntimeError(msg) from err
+
+
+def _handler(
+    event: Optional[Dict[str, Any]] = None,
+    context: Optional[Any] = None,
+) -> None:
+    event_data = _data_from_pubsub_event(event)
+    _LOGGER.debug('Processing event data: <%s>', event_data)
+    event_request = request_parser.to_event_request(event_data)
     project_id = _project_id_from_context(context)
     process_request.process(event_request, project_id)
-    _LOGGER.info('Finished CSV upload')
-    flask.jsonify(success=True)
+    _LOGGER.info('Processed event request: <%s>', event_request)
+
+
+def _data_from_pubsub_event(event: Dict[str, Any]) -> Dict[str, Any]:
+    # parse PubSub payload
+    if not isinstance(event, dict):
+        raise TypeError(f'Event must be of type {dict.__name__}. Got <{event}>({type(event)})')
+    event_data = event.get('data')
+    if event_data is None:
+        raise ValueError(f'Event data is None. Event: <{event}>')
+    try:
+        decoded_event_data = base64.b64decode(event_data).decode('utf-8')
+        result = json.loads(decoded_event_data)
+    except Exception as err:
+        msg = f'Cloud not parse PubSub event data. Event: <{event_data}>. Error: {err}'
+        _LOGGER.critical(msg)
+        raise RuntimeError(msg) from err
+    # adds timestamp
+    utc_ts: int = calendar.timegm(datetime.utcfromtimestamp(event.timestamp).utctimetuple())
+    # field name from bq_sampler.request.EventRequest
+    result['request_timestamp'] = utc_ts
+    return result
 
 
 def _project_id_from_context(context: Optional[Any] = None) -> Optional[str]:
